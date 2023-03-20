0fc3e075820d95822316bd940f22783751debb8f
Muchnik Illia <muchnik.illia.js@gmail.com>
2023-03-20 13:36:45 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+import { Configuration, OpenAIApi } from 'openai'
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY
+  const [inputText, setInputText] = useState('')
+  const [inputTextArea, setInputTextArea] = useState('')
+  const [isLoading , setIsLoading] = useState(false)
+
+  const openai = new OpenAIApi(
+    new Configuration({
+      apiKey,
+    })
+  )
+
+  const handleClick = async () => {
+    setIsLoading(true)
+    const res = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: inputText }],
+    })
+    setIsLoading(false)
+    const answer = res.data.choices[0].message.content
+    setInputTextArea(answer)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input value={inputText} onChange={e => setInputText(e.target.value)} />
+        <button onClick={handleClick}>Ok</button>
+      </div>
+      {
+        isLoading?<h1>Loading....</h1> :  <textarea value={inputTextArea} disabled style={{height: '80vh', width:'100vw'}}/>
+      }
+     
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
